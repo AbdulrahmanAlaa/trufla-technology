48957a21e2dbe75763f57cd84e2f6fbfbb6c88a4
abdulrahman alaa <abdelraman.bashir@vodafone.com>
2018-06-29 14:37:33 +0200

    adding store functionality

---
+++ index.spec.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('index module', () => {
+    it('should export a function', () => {
+        // No valid commands
+        const commands = ['node-path','index-path'];
+      //  console.log(process.stdout);
+        expect(1).to.eq(1);
+    });
+});
